19dc0beb93b574b6635922130a277b1dd7ab037f
Max Van Aken <mtvanaken@gmail.com>
2019-11-28 15:59:12 +0000

    WIP - reddit scraper

--- src/scrapers/reddit-scraper.js
+++ src/scrapers/reddit-scraper.js
@@ -1,14 +1,21 @@
 const axios = require('axios')
+const cheerio = require('cheerio')
+
 
 class RedditScraper {
     constructor(startingURL) {
         this._startingURL = startingURL
         this._urlQueue = [{url: this._startingURL, visited: false}]
     }
-
-    findLinks(url) {
+    
+    findLinks(pageData) {
         let links = []
-    } 
+        const $ = cheerio.load(pageData.data);
+        // $('body[class=_1oQyIsiPHYt6nx7VOmd1sz]').each((index, value) => {
+        //     let link = $(value).attr('href')
+        //    links.push(value)
+        // })
+    }
 
     async fetchPage(url) {
         try {
--- src/tests/reddit-scraper.test.js
+++ src/tests/reddit-scraper.test.js
@@ -4,14 +4,14 @@ describe('Reddit Scraper', () => {
     let scraper
 
     beforeEach(() => {
-        scraper = new RedditScraper('https://www.reddit.com/r/listentous/top/?sort=top&t=month')
+        scraper = new RedditScraper('https://new.reddit.com/r/listentous/top/?t=month')
     })
 
     test('Adds starting URL to queue', () => {
         expect(scraper._urlQueue).toEqual(
             [
                 {
-                    url: 'https://www.reddit.com/r/listentous/top/?sort=top&t=month',
+                    url: 'https://new.reddit.com/r/listentous/top/?t=month',
                     visited: false
                 }
             ]
@@ -21,15 +21,17 @@ describe('Reddit Scraper', () => {
     describe('Fetching page data', () => {
 
         test('Fetches page successfully', async () => {
-            let pageData = await scraper.fetchPage('https://www.reddit.com/r/listentous/top/?sort=top&t=month')
+            setTimeout(20000)
+            let pageData = await scraper.fetchPage('https://new.reddit.com/r/listentous/top/?t=month')
             expect(pageData.status).toEqual(200)
         })
     })
 
     describe('Gather data on page', () => {
 
-        test('Find links on page', () => {
-            let pageData = scraper.fetchPage('https://www.reddit.com/r/listentous/top/?sort=top&t=month')
+        test('Find links on page', async () => {
+            setTimeout(20000)
+            let pageData = await scraper.fetchPage('https://new.reddit.com/r/listentous/top/?t=month')
             let linksArr = scraper.findLinks(pageData)
             expect(linksArr.length).toBeGreaterThan(1)
         })
